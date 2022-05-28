7028d34b75acc203da640da0baa68e15e656bae5
Calos <fxiaoyu97@qq.com>
2022-05-28 18:50:58 +0800

    Create T.java

---
+++ src/main/java/com/calos/thread/c_017/T.java
@@ -0,0 +1,40 @@
+package com.calos.thread.c_017;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 锁定某对象o，如果o的属性发生改变，不影响锁的使用
+ * 但是如果o变成另外一个对象，则锁定的对象发生改变
+ * 应避免将锁定对象的引用变成另外的对象
+ *
+ * 解决方法：锁的对象添加final
+ */
+public class T {
+   /*final*/ Object o = new Object();
+
+    void m() {
+        synchronized (o) {
+            while (true) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        T t = new T();
+        new Thread(t::m, "t1").start();
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Thread t2 = new Thread(t::m, "t2");
+        t.o = new Object();
+        t2.start();
+    }
+}
